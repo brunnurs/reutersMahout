d5f97e0992df860c25af3aebea034bc4e852516c
Ursin Brunner <ursin.brunner@gmail.com>
2013-06-11 13:07:06 -0400

    created DTO for reuters messages

---
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/ReutersMessage.java
@@ -0,0 +1,21 @@
+package com.zuehlke.reuters.mahout;
+
+public class ReutersMessage {
+
+	private String topic;
+	private String message;
+	
+	public String getTopic() {
+		return topic;
+	}
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
