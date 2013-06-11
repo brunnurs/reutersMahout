2248f75f3417319352bdce5fa28fb94de0ad9f4a
Ursin Brunner <ursin.brunner@gmail.com>
2013-06-11 12:17:44 -0400

    create test structure

---
+++ reutersMahout/src/test/java/com/zuehlke/reuters/mahout/importer/ReutersDataImporterTest.java
@@ -0,0 +1,14 @@
+package com.zuehlke.reuters.mahout.importer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReutersDataImporterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
