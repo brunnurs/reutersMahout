1373f59fb3551721a89743275cac85c6ccec1ccf
gianba <gmbaschera@gmail.com>
2013-06-12 06:18:43 -0400

    feature collector added

---
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/FeatureCollector.java
@@ -0,0 +1,13 @@
+package com.zuehlke.reuters.mahout.features;
+
+import java.util.Set;
+
+import org.apache.mahout.math.Vector;
+
+public class FeatureCollector {
+	private Set<Feature> features;
+	
+	public Vector extractFeatures(String text){
+		return null;
+	}
+}
