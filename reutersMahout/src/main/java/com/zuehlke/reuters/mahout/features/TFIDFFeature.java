d500af73c7cec24738e31347ecbe9ed4e7addf4f
cloudera <cloudera@localhost.localdomain>
2013-06-12 05:37:33 -0400

    added numeric literals count

--- reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/TFIDFFeature.java
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/TFIDFFeature.java
@@ -7,19 +7,12 @@ import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.TermAttribute;
-import org.apache.lucene.util.Version;
-import org.apache.mahout.math.RandomAccessSparseVector;
 import org.apache.mahout.math.Vector;
-import org.apache.mahout.vectorizer.encoders.AdaptiveWordValueEncoder;
 import org.apache.mahout.vectorizer.encoders.ContinuousValueEncoder;
-import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
-import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
 
-public class TFIDFFeature implements Feature {
+public class TFIDFFeature extends AbstractFeature {
 	
 	private static final String WORD_COUNT_FILE = "wordCount.txt";
 	Map<String, Double> idf;
@@ -35,7 +28,6 @@ public class TFIDFFeature implements Feature {
 
 	public void extract( String text, Vector vector ) {
 		ContinuousValueEncoder encoder = new ContinuousValueEncoder("td-idf");
-		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);     
 		Map<String, Integer> wordCounts = new HashMap<String, Integer>();
 		
 		StringReader in = new StringReader( text );
@@ -63,8 +55,7 @@ public class TFIDFFeature implements Feature {
 			}
 			encoder.addToVector("tfidf", tfidf, vector);   
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("IOError: " + TFIDFFeature.class);
 		}
 
 		
--- reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/WordCountFeature.java
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/WordCountFeature.java
@@ -3,22 +3,16 @@ package com.zuehlke.reuters.mahout.features;
 import java.io.IOException;
 import java.io.StringReader;
 
-import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.TermAttribute;
-import org.apache.lucene.util.Version;
-import org.apache.mahout.math.RandomAccessSparseVector;
-import org.apache.mahout.math.SequentialAccessSparseVector;
 import org.apache.mahout.math.Vector;
 import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
 import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
 
-public class WordCountFeature implements Feature {
+public class WordCountFeature extends AbstractFeature {
 
 	public void extract(String text, Vector vector) {
-		FeatureVectorEncoder encoder = new StaticWordValueEncoder("text");
-		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);     
+		FeatureVectorEncoder encoder = new StaticWordValueEncoder("text"); 
 
 		StringReader in = new StringReader(text);
 		TokenStream ts = analyzer.tokenStream("body", in);
@@ -32,10 +26,8 @@ public class WordCountFeature implements Feature {
 			  encoder.addToVector(w, 1, vector);                                 
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("IOError: " + WordCountFeature.class);
 		}
-		System.out.printf("%s\n", new SequentialAccessSparseVector(vector));
 
 	}
 
