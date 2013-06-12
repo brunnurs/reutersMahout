f29114642a8ab4c48a234286977dfa7f02092114
gianba <gmbaschera@gmail.com>
2013-06-12 04:18:35 -0400

    Word count feature added

--- reutersMahout/src/main/java/com/zuehlke/reutersmahout/features/WordCountFeature.java
+++ reutersMahout/src/main/java/com/zuehlke/reutersmahout/features/WordCountFeature.java
@@ -9,18 +9,18 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.apache.lucene.util.Version;
 import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.SequentialAccessSparseVector;
 import org.apache.mahout.math.Vector;
-import org.apache.mahout.vectorizer.encoders.AdaptiveWordValueEncoder;
 import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
 import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
 
-public class WordAsCategoryFeature implements Feature {
+public class WordCountFeature implements Feature {
 
-	public void extract( String text, Vector vector ) {
-		FeatureVectorEncoder encoder = new AdaptiveWordValueEncoder("text");
+	public void extract(String text, Vector vector) {
+		FeatureVectorEncoder encoder = new StaticWordValueEncoder("text");
 		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);     
 
-		StringReader in = new StringReader( text );
+		StringReader in = new StringReader(text);
 		TokenStream ts = analyzer.tokenStream("body", in);
 		TermAttribute termAtt = ts.addAttribute(TermAttribute.class);
 
@@ -35,6 +35,7 @@ public class WordAsCategoryFeature implements Feature {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.printf("%s\n", new SequentialAccessSparseVector(vector));
 
 	}
 
---
+++ reutersMahout/src/test/java/WordCountFeatureTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.junit.Test;
+
+import com.zuehlke.reutersmahout.features.WordCountFeature;
+
+
+public class WordCountFeatureTest {
+
+	@Test
+	public void test() {
+		WordCountFeature wordCountFeature = new WordCountFeature();
+		Vector v1 = new RandomAccessSparseVector(10);                   
+		wordCountFeature.extract("two words", v1);
+	}
+
+}
