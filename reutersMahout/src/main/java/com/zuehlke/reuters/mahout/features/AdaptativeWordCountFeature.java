94d57489381aecab54581f57e793272ee3979e15
Fran <fcoblaya@gmail.com>
2013-06-13 06:41:56 -0400

    added adaptative word count

---
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/AdaptativeWordCountFeature.java
@@ -0,0 +1,34 @@
+package com.zuehlke.reuters.mahout.features;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.vectorizer.encoders.AdaptiveWordValueEncoder;
+import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
+
+public class AdaptativeWordCountFeature extends AbstractFeature {
+
+	public void extract(String text, Vector vector) {
+		FeatureVectorEncoder encoder = new AdaptiveWordValueEncoder("text"); 
+
+		StringReader in = new StringReader(text);
+		TokenStream ts = analyzer.tokenStream("body", in);
+		TermAttribute termAtt = ts.addAttribute(TermAttribute.class);
+
+		try {
+			while (ts.incrementToken()) {
+			  char[] termBuffer = termAtt.termBuffer();
+			  int termLen = termAtt.termLength();
+			  String w = new String(termBuffer, 0, termLen);                 
+			  encoder.addToVector(w, 1, vector);                                 
+			}
+		} catch (IOException e) {
+			System.out.println("IOError: " + AdaptativeWordCountFeature.class);
+		}
+
+	}
+
+}
