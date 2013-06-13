f4df8402387cb2d8263d08ac94ab65c4ec57f579
Thomas Scheuchzer <thomas.scheuchzer@gmx.net>
2013-06-13 07:59:44 -0400

    crossvalidation

--- reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/FeatureCollector.java
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/features/FeatureCollector.java
@@ -1,8 +1,6 @@
 package com.zuehlke.reuters.mahout.features;
 
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.apache.mahout.math.RandomAccessSparseVector;
@@ -11,18 +9,16 @@ import org.apache.mahout.math.Vector;
 public class FeatureCollector {
 	public static final int VECTOR_SIZE = 1000;
 	private static Set<Feature> features = new HashSet<Feature>();
-	private static Map<String, List<String>> categoryWords;
 	
 	static{
 		features.add( new NumberCountFeature() );
-		features.add( new WordCountFeature(categoryWords) );
+		features.add( new WordCountFeature() );
 		features.add( new BiasFeature() );
 		features.add( new CurrencyCountFeature() );
 		features.add( new AdaptativeWordCountFeature() );
 	}
 
-	public FeatureCollector(Map<String, List<String>> categoryWords){
-		this.categoryWords = categoryWords;
+	public FeatureCollector(){
 	}
 	
 	public Vector extractFeatures(String text){
