f511bc6419cda4162aa4406bb6628ba35fc121c0
Ursin Brunner <ursin.brunner@gmail.com>
2013-06-13 09:12:03 -0400

    MessageExtractor will now set all message-topic's to NONE if this category occurs less than 500 times

--- reutersMahout/src/main/java/com/zuehlke/reuters/mahout/importer/TopicStatistic.java
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/importer/TopicStatistic.java
@@ -1,6 +1,10 @@
 package com.zuehlke.reuters.mahout.importer;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +29,20 @@ public class TopicStatistic {
 		return categoryStatistics;
 	}
 	
+	public void printStatistics() {
+		
+		MapUtil mapUtil = new MapUtil();
+		Map<String, Integer> sortedMap = mapUtil.sortByValue(categoryStatistics);
+		
+		for (String key : sortedMap.keySet()) {
+			System.out.println("Topic: "+key + " Count:"+sortedMap.get(key));
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+	
 	public static TopicStatistic createNewStatistic(List<ReutersMessage> messages) {
 		
 		TopicStatistic newStatistic = new TopicStatistic();
@@ -36,6 +54,29 @@ public class TopicStatistic {
 		return newStatistic;
 		
 	}
-
 	
+
+	class MapUtil
+	{
+	    public <K, V extends Comparable<? super V>> Map<K, V> 
+	        sortByValue( Map<K, V> map )
+	    {
+	        List<Map.Entry<K, V>> list =
+	            new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+	        Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+	        {
+	            public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+	            {
+	                return (o1.getValue()).compareTo( o2.getValue() );
+	            }
+	        } );
+
+	        Map<K, V> result = new LinkedHashMap<K, V>();
+	        for (Map.Entry<K, V> entry : list)
+	        {
+	            result.put( entry.getKey(), entry.getValue() );
+	        }
+	        return result;
+	    }
+	}
 }
