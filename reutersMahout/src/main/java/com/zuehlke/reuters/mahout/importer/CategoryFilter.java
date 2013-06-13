3a37a66734641ba9f1c0bbe9fd958f1064315417
Ursin Brunner <ursin.brunner@gmail.com>
2013-06-13 07:54:23 -0400

    created filter for category

--- reutersMahout/src/main/java/com/zuehlke/reuters/mahout/importer/CategoryFilter.java
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/importer/CategoryFilter.java
@@ -1,5 +1,23 @@
 package com.zuehlke.reuters.mahout.importer;
 
+import java.util.List;
+
+import com.zuehlke.reuters.mahout.ReutersMessage;
+
 public class CategoryFilter {
 
+	private final static String NON_TOPIC = "NONE";
+
+	public void reduceMessagesToMostImportantTopics(TopicStatistic topicStatistic,List<ReutersMessage> allMessages,int limitCount) {
+		
+		for (ReutersMessage message : allMessages) {
+			
+			int countForThatTopic = topicStatistic.getCategoryStatistics().get(message.getTopic());
+			if(countForThatTopic < limitCount) {
+				message.setTopic(NON_TOPIC);
+			}
+			
+		}
+		
+	}
 }
--- reutersMahout/src/main/java/com/zuehlke/reuters/mahout/importer/TopicStatistic.java
+++ reutersMahout/src/main/java/com/zuehlke/reuters/mahout/importer/TopicStatistic.java
@@ -1,6 +1,7 @@
 package com.zuehlke.reuters.mahout.importer;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.zuehlke.reuters.mahout.ReutersMessage;
@@ -22,6 +23,18 @@ public class TopicStatistic {
 	
 	public Map<String, Integer> getCategoryStatistics() {
 		return categoryStatistics;
+	}
+	
+	public static TopicStatistic createNewStatistic(List<ReutersMessage> messages) {
+		
+		TopicStatistic newStatistic = new TopicStatistic();
+		
+		for (ReutersMessage reutersMessage : messages) {
+			newStatistic.addToStatistics(reutersMessage);
+		}
+		
+		return newStatistic;
+		
 	}
 
 	
